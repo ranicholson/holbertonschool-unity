eada3ec11d26d5bf3ee39be5c2dca047ea2ee453
Allen Nicholson <66395341+ranicholson@users.noreply.github.com>
2021-09-07 12:00:10 -0500

    Task 8 and 9 updates

---
+++ 0x05-unity-assets_models_textures/Assets/Scripts/Timer.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    private int minutes = 0;
+    private float seconds = 0.00f;
+    private float totalTime = 0f;
+    public Text TimerText;
+
+	
+    void Update () 
+    {
+        totalTime += Time.deltaTime;
+        seconds = totalTime % 60f;
+        minutes = (int) totalTime / 60;
+        TimerText.text = minutes.ToString() + ":" + seconds.ToString("00.00");
+	}
+}
